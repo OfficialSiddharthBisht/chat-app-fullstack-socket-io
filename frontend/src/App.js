da4dff53069136e9c5b47c433626e997ae5710a9
Siddharth Bisht <officialsiddharthbisht@gmail.com>
2023-08-31 11:07:03 +0530

    feat: chat application fullstack complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,19 +5,29 @@ import { nanoid } from 'nanoid'
 
 // no dotenv
 const socket = io("http://localhost:5000");
-
+const userName = nanoid(4)
 function App() {
   const [message, setMessage] = useState("");
   const [chat, setChat] = useState([]);
 
   const sendChat = (e) => {
     e.preventDefault()
-    socket.emit("chat", { message })
+    socket.emit("chat", { message, userName })
     setMessage('')
   }
+  useEffect(() => {
+    socket.on("chat", (payload) => {
+      setChat([...chat, payload])
+    })
+  })
   return (
     <div className="App">
       <h1>Chatty App</h1>
+      {chat.map((payload, index) => {
+        return (
+          <p id='chatColor' key={index}>{payload.message} <span>{payload.userName}</span></p>
+        )
+      })}
       <form onSubmit={sendChat}>
         <input
           type='text'
