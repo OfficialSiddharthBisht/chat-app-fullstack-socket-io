b84f838c21bcd4eb3c00e0257b02300b9abb9007
Siddharth Bisht <officialsiddharthbisht@gmail.com>
2023-08-31 10:51:53 +0530

    feat: fronted design , fix: backend cors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import io from 'socket.io-client'
+import { nanoid } from 'nanoid'
+
+// no dotenv
+const socket = io("http://localhost:5000");
 
 function App() {
+  const [message, setMessage] = useState("");
+  const [chat, setChat] = useState([]);
+
+  const sendChat = (e) => {
+    e.preventDefault()
+    socket.emit("chat", { message })
+    setMessage('')
+  }
   return (
     <div className="App">
       <h1>Chatty App</h1>
+      <form onSubmit={sendChat}>
+        <input
+          type='text'
+          name='chat'
+          placeholder='send text'
+          value={message}
+          onChange={(e) => {
+            setMessage(e.target.value)
+          }} />
+        <button type='submit' >Send </button>
+      </form>
     </div>
   );
 }
